742e3721cff3a2e13b1141928961ef1d67ca1ceb
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-13 22:17:18 +0700

    Binh edit contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Routes} from "react-router-dom"
+// import Popup from './file jsx/Contact file/popupcontact';
+import Contact from './file jsx/Contact file/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <nav>
+          <Link to="/contact">Contact</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </header>
     </div>
   );
 }
