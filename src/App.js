1ef1ca8d8eacc58b6a9ac77398a3f788eb46377e
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-15 08:55:29 +0700

    Binh them homepage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contact from "./file jsx/Contact file/Contact";
 import Login from "./file jsx/Login file/Login";
 import SearchButton from "./file jsx/Search/Search";
 import Menu from "./file jsx/Dropdown/MenuDropDown";
+import Home from "./file jsx/FoodHome/HomePage";
 
 function App() {
   return (
@@ -14,11 +15,13 @@ function App() {
         <nav className="navheader">
           <Menu />
           <Link to="/SearchButton">Search</Link>
+          <Link to="/Home">Food Shop</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/login">Login</Link>
         </nav>
         <Routes>
           <Route path="/SearchButton" element={<SearchButton />} />
+          <Route path="Home" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="login" element={<Login />} />
         </Routes>
