8f6b16d149fff1ee2afa07575681647d54a85ff8
TranVi21 <tranvy20112002@gmail.com>
2024-03-15 09:07:58 +0700

    vi chinh sua css

--- src/file jsx/Search/List.jsx
+++ src/file jsx/Search/List.jsx
@@ -6,7 +6,7 @@ function Search({ Name, Price, Image }) {
         <span className="name"> {Name}</span>
         <span className="price">Price: {Price}</span>
       </p>
-      <button className="view">View Detail</button>
+      <button className="view">Detail</button>
     </div>
   );
 }
