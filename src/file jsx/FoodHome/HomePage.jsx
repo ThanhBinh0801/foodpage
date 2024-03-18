58e0353b7ee84faee659d999cb307b76d9714365
XuanNghia020518 <tranxuannghia020518@gmail.com>
2024-03-18 11:26:53 +0700

    nghia

--- src/file jsx/FoodHome/HomePage.jsx
+++ src/file jsx/FoodHome/HomePage.jsx
@@ -8,65 +8,76 @@ function Home() {
           <div className="Healthyimgbox">
             <img src="./imgHome/anhmenuchinh.png" alt="" />
           </div>
-          <div className="Healthytitle"><span >Healthy Snacks</span></div>
         </Link>
         <div className="detail">
-          <Link to="/Smooth">
-            <span>Smoothies</span>
-            <span>
-              <img src="./imgHome/anhSmoothies.png" alt="" />
-            </span>
-          </Link>
-          <Link to="/Cereal">
-            <span>Cereal</span>
-            <span>
-              <img src="./imgHome/anhCereal.png" alt="" />
-            </span>
-          </Link>
+          <div>
+            <div>
+              <Link to="/Smooth">
+                <span>
+                  <img src="./imgHome/anhSmoothies.png" alt="" />
+                </span>
+              </Link>
+            </div>
+            <div>
+              <Link to="/Cereal">
+                <span>
+                  <img src="./imgHome/anhCereal.png" alt="" />
+                </span>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div> <br />
+      </div>{" "}
+      <br /> <br />
       <div>
         <Link>
-          <span>Snacks for Kids</span>
           <span>
             <img src="./imgHome/anhSnacksforkids.png" alt="" />
           </span>
         </Link>
         <div className="detail">
-          <Link to="/Cakes">
-            <span>Cakes</span>
-            <span>
-              <img src="./imgHome/anhCakes.png" alt="" />
-            </span>
-          </Link>
-          <Link to="/Yogurt">
-            <span>Yogurt</span>
-            <span>
-              <img src="./imgHome/anhYogurt.png" alt="" />
-            </span>
-          </Link>
+          <div>
+            <div>
+              <Link to="/Cakes">
+                <span>
+                  <img src="./imgHome/anhCakes.png" alt="" />
+                </span>
+              </Link>
+            </div>
+            <div>
+              <Link to="/Yogurt">
+                <span>
+                  <img src="./imgHome/anhYogurt.png" alt="" />
+                </span>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div> <br />
+      </div>{" "}
+      <br /> <br />
       <div>
         <Link>
-          <span>East on stomach snacks</span>
           <span>
             <img src="./imgHome/anhEasyonStomachSnacks.png" alt="" />
           </span>
         </Link>
         <div className="detail">
-          <Link to="/FoodForEoss">
-            <span>Foods for EOSS</span>
-            <span>
-              <img src="./imgHome/anhFoodforEOSS.png" alt="" />
-            </span>
-          </Link>
-          <Link to="/DrinksForEoss">
-            <span>Drinks</span>
-            <span>
-              <img src="./imgHome/anhDrinks.png" alt="" />
-            </span>
-          </Link>
+          <div>
+            <div>
+              <Link to="/FoodForEoss">
+                <span>
+                  <img src="./imgHome/anhFoodforEOSS.png" alt="" />
+                </span>
+              </Link>
+            </div>
+            <div>
+              <Link to="/DrinksForEoss">
+                <span>
+                  <img src="./imgHome/anhDrinks.png" alt="" />
+                </span>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
