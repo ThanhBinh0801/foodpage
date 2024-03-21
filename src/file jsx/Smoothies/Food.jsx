54c027f230cd47b7ea129e56cf9a7420bd1921ed
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-21 16:18:50 +0700

    b thay doi trong food.jsx cua smoothies

--- src/file jsx/Smoothies/Food.jsx
+++ src/file jsx/Smoothies/Food.jsx
@@ -7,9 +7,12 @@ function Food(props) {
         className="cacsanphamchinh"
         onClick={() => navigate(`/smooth/${props.id}`)}
       >
-        <img src={props.img} alt="Hinh Dai dien"/>
-        <div className="tenthanhphan">{props.Name}</div>     
-      </div> <br />
+        <div className="tenthanhphan">{props.Name}</div>
+        <div>
+          <img src={props.img} alt="Hinh Dai dien" />
+        </div>
+      </div>{" "}
+      <br />
     </>
   );
 }
